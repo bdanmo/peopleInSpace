d756931d2e95e07ac409f7411830b9177b0e4e5b
Brendan Moran <b.daniel.moran@gmail.com>
2020-07-14 21:11:40 -0400

    Built in easter eggs and a guaranteed failed call to demonstrate workaround.

--- js/async-await.js
+++ js/async-await.js
@@ -1,12 +1,45 @@
 const astrosUrl = 'http://api.open-notify.org/astros.json';
 const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
+const easterEggs = [{
+  craft: "Death Star",
+  name: "Darth Vader"
+}, {
+  craft: "Decapodian Warship",
+  name: "Zoidberg"
+}, {
+  craft: "Some Ship",
+  name: "Some Guy"
+}];
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 async function getPeopleInSpace(url) {
   const peopleResponse = await fetch(url);
-  const peopleJSON = await peopleListRes.json();
+  const peopleJSON = await peopleResponse.json();
+  peopleJSON.people.push(...easterEggs)
   console.log(peopleJSON);
+
+  const profiles = peopleJSON.people.map(async (person) => {
+    const craft = person.craft;
+    const profileResponse = await fetch(wikiUrl + person.name);
+    const profileJSON = await profileResponse.json();
+
+    if (profileJSON.pageid) {
+      return {
+        ...profileJSON,
+        craft
+      }
+    } else {
+      return {
+        ...profileJSON,
+        craft,
+        name: person.name,
+        search: `https://www.google.com/search?q=${encodeURI(person.name)}`,
+      }
+    }
+  });
+
+  return Promise.all(profiles);
 }
 
 function generateHTML(data) {
@@ -22,7 +55,7 @@ function generateHTML(data) {
     if (person.pageid) {
       const section = createSection();
       section.innerHTML = `
-        <img src=${person.thumbnail.source}>
+        <img src=${person.thumbnail ? person.thumbnail.source : `https://via.placeholder.com/150x300.png?text=${encodeURI(person.title)}`}>
         <span>${person.craft}</span>
         <h2>${person.title}</h2>
         <p>${person.description}</p>
@@ -41,7 +74,9 @@ function generateHTML(data) {
 
 }
 
-btn.addEventListener('click', (event) => {
+btn.addEventListener('click', async (event) => {
   event.target.textContent = 'Loading...'
-
+  const profiles = await getPeopleInSpace(astrosUrl);
+  generateHTML(profiles);
+  event.target.remove();
 });
